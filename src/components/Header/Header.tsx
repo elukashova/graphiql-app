3020a982ccc4ff513e6d1f186074035a821b29bb
Karinaguseva <karinguseva@gmail.com>
2023-05-05 10:28:40 +0300

    feat: make sticky and animation header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Header.module.css';
 import Logo from './components/Logo/Logo';
 import ButtonLink from './components/ButtonLink/ButtonLink';
 
 const Header = (): JSX.Element => {
+  const [scroll, setScroll] = useState(false);
+
+  const scrollHeader = () => {
+    if (window.scrollY >= 50) setScroll(true);
+    else setScroll(false);
+  };
+
+  window.addEventListener('scroll', scrollHeader);
+
   return (
-    <header className={styles.header}>
+    <header className={!scroll ? styles.header : `${styles.header} ${styles.active}`}>
       <div className={styles.wrapper}>
         <Logo />
         <div className={styles.buttons}>
