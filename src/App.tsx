0fbfaf3c4112b3430f7ac10a0b74ed2d18dc36e4
Trickypie <klimova.pr@gmail.com>
2023-05-05 17:23:40 +0300

    refactor: try to resolve problem with deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,18 @@
-/* import GraphiqlPage from './pages/Graphiql/Graphiql';
- */ /* import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Layout from './pages/Layout';
-import WelcomePage from './pages/Welcome/Welcome';
-
-const App = (): JSX.Element => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<WelcomePage />} />
-          {<Route path="editor" element={<GraphiqlPage />} />}
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-};
-
-export default App; */
-
 import GraphiqlPage from './pages/Graphiql/Graphiql';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './pages/Layout';
 import WelcomePage from './pages/Welcome/Welcome';
+import NotFound from './pages/NotFound/NotFound';
 
 const App = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="editor" element={<GraphiqlPage />} />
-          <Route index element={<WelcomePage />} />
+        <Route path="/" element={<Layout content={<WelcomePage />} />}>
+          <Route path="/editor" element={<Layout content={<GraphiqlPage />} />} />
+          <Route path="*" element={<Layout content={<NotFound />} />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 import styles from './Layout.module.css';
 
-const Layout = (): JSX.Element => {
+type props = {
+  content: JSX.Element;
+};
+
+const Layout = (props: props): JSX.Element => {
   return (
     <>
       <Header />
-      <main className={styles.main}>
-        <Outlet />
-      </main>
+      <main className={`${styles.main}`}>{props.content}</main>
       <Footer />
     </>
   );
