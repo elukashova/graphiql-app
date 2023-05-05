5bfa45ffba04c118f628a711ae206c30c0db54fa
Trickypie <klimova.pr@gmail.com>
2023-05-06 00:32:43 +0300

    refactor: separate main page editor and response block in another component

--- src/pages/Graphiql/Graphiql.tsx
+++ src/pages/Graphiql/Graphiql.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import styles from './Graphiql.module.css';
 import Editor from './Editor/Editor';
-import Button from '../../components/Button/Button';
 
 const GraphiqlPage = (): JSX.Element => {
-  const handleSubmit = () => {
-    console.log('Button submit clicked');
-  };
-
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <div className={styles['editor-wrapper']}>
-          <section className={`${styles['editor-section']}`}>
-            <Editor />
-            <section className={`${styles.response}`}>
-              <textarea className={`${styles.textarea}`} disabled>
-                Best response ever
-              </textarea>
-            </section>
-          </section>
+          <Editor />
+          {/* <section className={`${styles.response}`}>
+              <textarea
+                className={`${styles.textarea}`}
+                disabled
+                defaultValue="Best response ever"
+              ></textarea>
+            </section> */}
+
           <button className={`${styles.docs}`} type="button">
             <img
               src="https://img.icons8.com/color/48/null/story-book.png"
@@ -28,8 +24,6 @@ const GraphiqlPage = (): JSX.Element => {
             />
           </button>
         </div>
-
-        <Button text="Submit" disabled={false} onClick={handleSubmit} />
       </div>
 
       <aside>
