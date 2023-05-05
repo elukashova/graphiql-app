8848c1b1678ea645dfc21b1201e0444682e5c834
Trickypie <klimova.pr@gmail.com>
2023-05-06 00:33:24 +0300

    refactor: add initial testing request

--- src/pages/Graphiql/Editor/Editor.tsx
+++ src/pages/Graphiql/Editor/Editor.tsx
@@ -1,13 +1,53 @@
-import React from 'react';
+import Button from '../../../components/Button/Button';
+import React, { useState } from 'react';
 import styles from './Editor.module.css';
 
 const Editor = (): JSX.Element => {
+  const url = 'https://rickandmortyapi.com/graphql';
+  const [response, setResponse] = useState('response');
+  const [formValue, setFormValue] = useState(
+    'query { characters(filter: { name: "Morty" }) { results { name status species } } }'
+  );
+
+  const makeRequest = async (query: string): Promise<string> => {
+    console.log('makeRequest');
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ query }),
+    });
+    const result = await res.json();
+    console.log(result);
+    setResponse(JSON.stringify(result));
+    return JSON.stringify(result);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    console.log('handleSubmit');
+    e.preventDefault();
+    makeRequest(formValue);
+  };
+
+  const handleFormChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setFormValue(e.target.value);
+  };
+
   return (
     <section className={`${styles['editor-wrapper']}`}>
-      <textarea
-        defaultValue={`query {\n  test {\n    id\n  }\n}`}
-        className={`${styles.textarea}`}
-      ></textarea>
+      <form className={`${styles['editor-section']}`} onSubmit={handleSubmit}>
+        <textarea
+          name="query"
+          value={formValue}
+          onChange={handleFormChange}
+          className={`${styles.textarea}`}
+        ></textarea>
+        <section className={`${styles.response}`}>
+          <textarea className={`${styles.textarea}`} disabled value={response}></textarea>
+        </section>
+        <Button text="Submit" type="submit" disabled={false} />
+      </form>
     </section>
   );
 };
