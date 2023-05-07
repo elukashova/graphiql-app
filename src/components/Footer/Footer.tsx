39717844148902eee7ccee4c57c2f81eb043d67f
Karinaguseva <karinguseva@gmail.com>
2023-05-07 12:56:53 +0300

    feat: add links to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,38 @@ import styles from './Footer.module.css';
 const Footer = (): JSX.Element => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.wrapper}>This is footer</div>
+      <div className={styles.wrapper}>
+        <div className={styles.gitHub}>
+          <a
+            className={styles.link}
+            href="https://github.com/elukashova"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Elukashova
+          </a>
+          <a
+            className={styles.link}
+            href="https://github.com/TrickyPie"
+            target="_blank"
+            rel="noreferrer"
+          >
+            TrickyPie
+          </a>
+          <a
+            className={styles.link}
+            href="https://github.com/Karinaguseva"
+            target="_blank"
+            rel="noreferrer"
+          >
+            KarinaGuseva
+          </a>
+        </div>
+        <div className={styles.year}>&copy; 2023</div>
+        <a className={styles.rss} href="https://rs.school/js/" target="_blank" rel="noreferrer">
+          rs school course js
+        </a>
+      </div>
     </footer>
   );
 };
