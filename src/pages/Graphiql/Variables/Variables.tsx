5bed65785504cff7c776842e257db15d62a27cb9
Trickypie <klimova.pr@gmail.com>
2023-05-11 00:58:12 +0300

    feat: add logic for opening and closing variables block in different case

--- src/pages/Graphiql/Variables/Variables.tsx
+++ src/pages/Graphiql/Variables/Variables.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from 'store/store';
 import styles from './Variables.module.css';
-import { setVariables, SetVariablesAction } from '../../../store/variablesReducer';
+import { setVariables } from '../../../store/variablesReducer';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
 const Variables: React.FC = (): JSX.Element => {
   const variables = useSelector((state: RootState) => state.variables.variables);
-
+  const [showVariables, setShowVariables] = useState(variables ? variables.trim() !== '' : false);
   const dispatch = useDispatch<AppDispatch>();
 
   function handleVariablesChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
@@ -16,18 +16,24 @@ const Variables: React.FC = (): JSX.Element => {
     dispatch(setVariables(newVariables));
   }
 
+  function changeShowVariables() {
+    const updatedShowStatus = !showVariables;
+    setShowVariables(updatedShowStatus);
+  }
+
   return (
     <div className={styles['docs-container']}>
-      <button>Variables</button>
-
-      <div className={styles.variablesPanel}>
-        <textarea
-          value={variables || ''}
-          onChange={(e) => handleVariablesChange(e)}
-          placeholder="Enter variables in JSON format"
-        />
-        <button>Close</button>
-      </div>
+      <button onClick={() => changeShowVariables()}>Variables</button>
+      {showVariables && (
+        <div className={styles.variablesPanel}>
+          <textarea
+            value={variables || ''}
+            onChange={(e) => handleVariablesChange(e)}
+            placeholder="Enter variables in JSON format"
+          />
+          <button onClick={() => changeShowVariables()}>Close</button>
+        </div>
+      )}
     </div>
   );
 };
