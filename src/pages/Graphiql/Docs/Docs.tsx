a605cd1b387758e925ac527398c441696a29bcf2
Karinaguseva <karinguseva@gmail.com>
2023-05-12 19:47:32 +0300

    feat: add props to modal window

--- src/pages/Graphiql/Docs/Docs.tsx
+++ src/pages/Graphiql/Docs/Docs.tsx
@@ -4,13 +4,16 @@ import React from 'react';
 import styles from './Docs.module.css';
 
 import useDocs from '../../../hooks/docsHook';
+import { useAppSelector } from '../../../store/hooks';
+import { selectDocs } from '../../../store/slices/docs';
+import ModalWindow from './components/modalWindow/modalWindow';
 
 const Docs: React.FC = (): JSX.Element => {
   const apiUrlFromStorage: string =
     localStorage.getItem('apiUrl') || 'https://rickandmortyapi.com/graphql';
 
   // метод запрашивает схему апи, которая была прописана в инпуте
-  const fetchSchema = async (): Promise<void> => {
+  const fetchSchema = async (): Promise<GraphQLSchema> => {
     const query: string = getIntrospectionQuery(); // graphQL-запрос для получения схемы
     const result: Response = await fetch(apiUrlFromStorage, {
       method: 'POST',
@@ -24,12 +27,14 @@ const Docs: React.FC = (): JSX.Element => {
     console.log('schemaJSON', schemaJSON); // выводит в формате json
     const schema: GraphQLSchema = buildClientSchema(schemaJSON.data);
     console.log('schema', schema); // а тут схема в формате, с которым (судя по всему) тебе надо будет работать
+    console.log(schema.getQueryType()?.getFields().character.description); // а тут схема в формате, с которым (судя по всему) тебе надо будет работать
+    return schema;
   };
 
   fetchSchema(); // вывела для проверки
 
   const { toggleDocs } = useDocs();
-
+  const { isDocs } = useAppSelector(selectDocs);
   return (
     <>
       <div className={styles['docs-container']}>
@@ -41,6 +46,7 @@ const Docs: React.FC = (): JSX.Element => {
           />
         </button>
       </div>
+      {isDocs && <ModalWindow schema={fetchSchema()} />}
     </>
   );
 };
--- src/pages/Graphiql/Docs/components/modalWindow/modalWindow.tsx
+++ src/pages/Graphiql/Docs/components/modalWindow/modalWindow.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import styles from './modalWindow.module.css';
+import { GraphQLSchema } from 'graphql';
 
-const ModalWindow = () => {
+type ModalWindowProps = {
+  schema: Promise<GraphQLSchema>;
+};
+
+const ModalWindow = ({ schema }: ModalWindowProps) => {
   return <h3 className={styles.docs}>Docs</h3>;
 };
 
