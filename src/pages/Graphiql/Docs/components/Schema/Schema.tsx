50076ead46be9a66b9d35fe1672e49ea0fbcfdb4
Karinaguseva <karinguseva@gmail.com>
2023-05-13 21:22:53 +0300

    refactor: delete schema

--- src/pages/Graphiql/Docs/components/Schema/Schema.tsx
+++ src/pages/Graphiql/Docs/components/Schema/Schema.tsx
@@ -3,11 +3,12 @@ import styles from './Schema.module.css';
 
 const Schema = () => {
   return (
-    <iframe
-      className={styles.modal}
-      src="../../../../doc/schema/index.html"
-      title="GraphQL documentation"
-    ></iframe>
+    <h3 className={styles.modal}>DOCS</h3>
+    // <iframe
+    //   className={styles.modal}
+    //   src="../../../../doc/schema/index.html"
+    //   title="GraphQL documentation"
+    // ></iframe>
   );
 };
 
