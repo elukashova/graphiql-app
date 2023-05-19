fc3feef37c7fe264b21c169027d060cf2a35483f
Trickypie <klimova.pr@gmail.com>
2023-05-19 15:43:23 +0300

    feat: update 404 page styles

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import styles from './NotFound.module.css';
+import notFound from '../../assets/404.png';
 
 const NotFound = (): JSX.Element => {
   return (
     <div className={styles.container}>
-      <h2>Page not found</h2>
+      <img className={styles.background} src={notFound} alt="" />
+      <p className={styles.text}>
+        Page not found.
+        <br />
+        Go to
+        <a href="/" className={styles.highlight}>
+          editor?
+        </a>
+      </p>
     </div>
   );
 };
