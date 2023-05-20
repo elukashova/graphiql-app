c33d4f70b957046b7fd9daed0f59d75baf5b3718
Trickypie <klimova.pr@gmail.com>
2023-05-20 23:06:58 +0300

    feat: update styles for 404 page and add adaptive

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import styles from './NotFound.module.css';
-import notFound from '../../assets/404.png';
+import notFound from '../../assets/auth-background2.png';
 
 const NotFound = (): JSX.Element => {
   return (
     <div className={styles.container}>
       <img className={styles.background} src={notFound} alt="" />
-      <p className={styles.text}>
+      <h2 className={styles.text}>
         Page not found.
         <br />
         Go to
         <a href="/" className={styles.highlight}>
           editor?
         </a>
-      </p>
+      </h2>
     </div>
   );
 };
