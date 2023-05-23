ae2d02b90cc5882dd5cee9fca8b9fe8c3fdcac81
Trickypie <klimova.pr@gmail.com>
2023-05-24 00:15:41 +0300

    feat: show method with arguments and return value data

--- src/pages/Graphiql/Docs/components/Schema/ListItem.tsx
+++ src/pages/Graphiql/Docs/components/Schema/ListItem.tsx
@@ -1,118 +1,52 @@
-import { GraphQLField, GraphQLFieldMap, GraphQLObjectType } from 'graphql';
+import {
+  GraphQLField,
+  GraphQLFieldMap,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLOutputType,
+  GraphQLScalarType,
+} from 'graphql';
 import React, { useState } from 'react';
 import styles from '../ComponentsSchema.module.css';
 
-interface ListItemProps {
-  data: GraphQLField<unknown, unknown, unknown>;
+export type Data = GraphQLField<unknown, unknown, unknown>;
+
+export interface ListItemProps {
+  data: Data;
+  getNameData: (data: Data) => void;
+  getTypeData: (type: GraphQLOutputType) => void;
 }
 
-interface ListItemProps {
-  data: GraphQLField<unknown, unknown, unknown>;
-}
-
-const ListItem: React.FC<ListItemProps> = ({ data }) => {
+const ListItem: React.FC<ListItemProps> = ({ data, getNameData, getTypeData }) => {
   const { name, type, args, description } = data;
   const lang = 'en';
-  const [showNameFields, setShowNameFields] = useState(false);
-  const [showTypeFields, setShowTypeFields] = useState(false);
-
   const handleName = () => {
-    setShowNameFields(!showNameFields);
+    getNameData(data);
   };
 
   const handleType = () => {
-    setShowTypeFields(!showTypeFields);
+    getTypeData(type);
   };
 
   return (
     <>
-      <div>
-        <p>
-          <span className={styles.title} onClick={handleName}>
-            {name}
-          </span>
+      <div className={styles['list-block']}>
+        <span className={styles.title} onClick={handleName}>
+          {name}
+        </span>
+        <p className={styles['return-value']}>
+          Return value:{' '}
           <span className={styles.type} onClick={handleType}>
-            ({type.toString()}) |
-            {description ? (
-              <p className={styles.description}>{JSON.parse(description ?? '{}')?.[lang]}</p>
-            ) : null}
+            {type.toString()}
           </span>
         </p>
-      </div>
-      <div>
-        {showNameFields && (
-          <>
-            <h4>Name: {name}</h4>
-            {args.length === 0 ? (
-              <p>No arguments</p>
-            ) : (
-              <ul>
-                {data.args.map((arg) => (
-                  <li key={arg.name}>
-                    {arg.name}: {arg.type.toString()}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </>
-        )}
+
+        {description ? (
+          <span className={styles.description}>{JSON.parse(description ?? '{}')?.[lang]}</span>
+        ) : null}
       </div>
     </>
   );
 };
 
 export default ListItem;
-
-/* <h5>Name:</h5>
-
-      <h5>Type:</h5>
-      <p>{type.toString()}</p>
-      <h5>Arguments:</h5>
-      {args.length === 0 ? (
-        <p>No arguments</p>
-      ) : (
-        <ul>
-          {data.args.map((arg) => (
-            <li key={arg.name}>
-              {arg.name}: {arg.type.toString()}
-            </li>
-          ))}
-        </ul>
-      )}
-      <h5>Description:</h5>
-      {description ? <p>{JSON.parse(description ?? '{}')?.[lang]}</p> : <p></p>} */
-
-/* import { ListItemData } from '../SchemaRoot/SchemaRoot'; */
-
-/* export declare type GraphQLFieldMap<TSource, TContext> = ObjMap<
-  GraphQLField<TSource, TContext>
->; */
-
-/* const ListItem: React.FC<GraphQLFieldMap> = () => {
-  const lang = 'eng';
-
-  return (
-    <div>
-      { <h5>Name:</h5>
-      <p>{name}</p>
-      <h5>Type:</h5>
-      <p>{type.toString()}</p>
-      <h5>Arguments:</h5>
-      {args.length === 0 ? (
-        <p>No arguments</p>
-      ) : (
-        <ul>
-          {args.map((arg) => (
-            <li key={arg.name}>
-              {arg.name}: {arg.type.toString()}
-            </li>
-          ))}
-        </ul>
-      )}
-      <h5>Description:</h5>
-      <p>{JSON.parse(description)[lang]}</p> }
-    </div>
-  );
-};
-
-export default ListItem; */
--- src/pages/Graphiql/Docs/components/SchemaRoot/SchemaRoot.tsx
+++ src/pages/Graphiql/Docs/components/SchemaRoot/SchemaRoot.tsx
@@ -1,52 +1,72 @@
-import React, { useState } from 'react';
-import { OnSelectRoot } from '../Schema/types/onSelect';
+import React, { useEffect, useState } from 'react';
 import styles from '../ComponentsSchema.module.css';
 import {
-  GraphQLInputType,
-  GraphQLNamedType,
+  GraphQLField,
+  GraphQLList,
   GraphQLNonNull,
   GraphQLObjectType,
   GraphQLOutputType,
-  GraphQLScalarType,
   GraphQLSchema,
-  IntrospectionSchema,
 } from 'graphql';
-import ListItem from '../Schema/ListItem';
+import ListItem, { Data } from '../Schema/ListItem';
 
 interface SchemaRootProps {
   schema: GraphQLSchema;
-  onSelect: OnSelectRoot;
 }
 
-/* type FieldType = GraphQLObjectType | GraphQLScalarType;
-
-type ArgType = GraphQLInputType | FieldType | GraphQLNonNull<ArgType>;
-
-type DefaultValue = string | number | boolean | null;
-
-interface GraphQLArgumentData {
-  name: string;
-  type: ArgType;
-  defaultValue?: DefaultValue;
-}
-
-export interface ListItemData {
-  name: string;
-  type: FieldType;
-  args: GraphQLArgumentData[];
-  description: string;
-}
- */
 export const SchemaRoot: React.FC<SchemaRootProps> = ({ schema }) => {
   const queryType = schema.getQueryType() as GraphQLObjectType;
-
-  console.log(schema, queryType);
+  const lang = 'en';
   const [showFields, setShowFields] = useState(false);
+  const [showName, setShowName] = useState(false);
+  const [nameData, setNameData] = useState<GraphQLField<unknown, unknown, unknown> | null>(null);
+  const [prevNameData, setNamePrevData] = useState<GraphQLField<unknown, unknown, unknown> | null>(
+    null
+  );
+  const [showType, setShowType] = useState(false);
+  const [typeData, setTypeData] = useState<GraphQLOutputType | null>(null);
+  const [prevTypeData, setTypePrevData] = useState<GraphQLOutputType | null>(null);
+
   const fields = queryType.getFields();
 
   const handleQueryTypeClick = () => {
     setShowFields(!showFields);
   };
+
+  const handleNameClick = (data: Data) => {
+    if (showType && prevNameData && JSON.stringify(data) === JSON.stringify(prevNameData)) {
+      setShowName(true);
+      setShowType(false);
+      setNameData(data);
+    } else if (!prevNameData || JSON.stringify(data) !== JSON.stringify(prevNameData)) {
+      setShowName(true);
+      setShowType(false);
+      setNameData(data);
+      setNamePrevData(data);
+    } else {
+      setShowName(!showName);
+    }
+  };
+
+  const handleTypeClick = (type: GraphQLOutputType) => {
+    if (showName && prevTypeData && JSON.stringify(type) === JSON.stringify(prevTypeData)) {
+      setShowType(true);
+      setShowName(false);
+      setTypeData(type);
+    } else if (!prevTypeData || type !== prevTypeData) {
+      setShowType(true);
+      setShowName(false);
+      setTypeData(type);
+      setTypePrevData(type);
+    } else {
+      setShowType(!showType);
+    }
+  };
+
+  useEffect(() => {
+    console.log('showName:', showName);
+    console.log('showType:', showType);
+  }, [showName, showType]);
 
   return (
     <div className={styles.schema}>
@@ -63,41 +83,68 @@ export const SchemaRoot: React.FC<SchemaRootProps> = ({ schema }) => {
         {showFields && (
           <>
             <h4>Method list:</h4>
-            <ul>
+            <ul className={styles['methods-list']}>
               {Object.keys(fields).map((fieldName) => (
                 <li key={fieldName}>
-                  <ListItem data={fields[fieldName]} />
+                  <ListItem
+                    data={fields[fieldName]}
+                    getNameData={handleNameClick}
+                    getTypeData={handleTypeClick}
+                  />
                 </li>
               ))}
             </ul>
           </>
         )}
       </div>
+      {showFields && showName && !showType && nameData && nameData.name && (
+        <div>
+          <>
+            <h4>{nameData.name}</h4>
+            <span>{JSON.parse(nameData.description ?? '{}')?.[lang]}</span>
+            {nameData.args && nameData.args.length === 0 ? (
+              <h5>No arguments</h5>
+            ) : (
+              <>
+                <h5>Arguments: </h5>
+                <ul className={styles['methods-list']}>
+                  {nameData.args &&
+                    nameData.args.map((arg) => (
+                      <li className={styles['arg-list']} key={arg.name}>
+                        <span className={styles.arg}>{arg.name}</span>
+                        <span className={styles.type}>({arg.type.toString()})</span>{' '}
+                        <span className={styles.description}>
+                          {JSON.parse(arg.description ?? '{}')?.[lang]}
+                        </span>
+                      </li>
+                    ))}
+                </ul>
+              </>
+            )}
+          </>
+        </div>
+      )}
+      {showFields && showType && !showName && typeData && (
+        <div>
+          <h4>{typeData.toString()}</h4>
+          {typeData instanceof GraphQLNonNull && typeData.ofType instanceof GraphQLObjectType && (
+            <>
+              {typeData.ofType.description && <p>{typeData.ofType.description}</p>}
+              <ul>
+                {Object.values(typeData.ofType.getFields()).map((field) => (
+                  <li className={styles['arg-list']} key={field.name}>
+                    <span className={styles.arg}>{field.name}</span>
+                    <span className={styles.type}>({field.type.toString()})</span>{' '}
+                    <span className={styles.description}>
+                      {JSON.parse(field.description || '{}')[lang]}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
+      )}
     </div>
   );
 };
-
-/* <ListItem key={queryType.name} props={fields} /> */
-
-/* interface SchemaRootProps {
-  schema: GraphQLSchema;
-  onSelect: OnSelectRoot;
-}
-
-export const SchemaRoot: React.FC<SchemaRootProps> = ({ onSelect, schema }) => {
-  console.log(schema.name);
-
-  return (
-    <div>
-      <h4>Root Types:</h4>
-      <div>
-        <p className={styles.query}>
-          query:
-          <span className={styles.title} onClick={() => onSelect(schema)}>
-            {schema}
-          </span>
-        </p>
-      </div>
-    </div>
-  );
-}; */
